4d7406e7b473f7372faf18e6d332432d9359e8b0
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-03 15:28:42 -0300

    Criando os elementos que compõem a página Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,59 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity } from 'react-native'
+import { Logo } from '../../components/Logo'
+import { Ionicons } from '@expo/vector-icons'
 
 export function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Página Home</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Logo />
+      <Text style={styles.title}>Encontre a receita </Text>
+      <Text style={styles.title}>que combina com você</Text>
+      <View style={styles.form}>
+        <TextInput 
+          placeholder='Digite o nome da comida...'
+          style={styles.input}
+        />
+        <TouchableOpacity>
+          <Ionicons name='search' size={28} color='#4cbe6c' />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'red'
-  }
+    flex: 1,
+    backgroundColor: '#f3f9ff',
+    paddingTop: 36,
+    paddingStart: 14,
+    paddingEnd: 14,
+  },
+
+  title: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#0e0e0e',
+  },
+
+  form: {
+    backgroundColor: '#fff',
+    width: '100%',
+    borderRadius: 8,
+    marginTop: 16,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: '#ececec',
+    paddingLeft: 8,
+    paddingRight: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  input: {
+    width: '90%',
+    height: 54,
+    maxWidth: '90%'
+  },
 })
