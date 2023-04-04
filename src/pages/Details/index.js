cff99a57eb2792bdca4f8517694bf4aa329d9b83
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-04 15:34:41 -0300

    criando o header da página de Details

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,9 +1,31 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { useLayoutEffect } from 'react'
+import { View, Text, StyleSheet, Pressable } from 'react-native'
+import { useRoute, useNavigation } from '@react-navigation/native'
+import { Entypo } from '@expo/vector-icons'
 
 export function Details() {
+  const route = useRoute()
+  const navigation = useNavigation()
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: route.params?.data ? route.params?.data.name : 'Detalhes da receita',
+      headerRight: () => (
+        <Pressable onPress={() => console.log('Testando')}>
+          <Entypo 
+            name='heart' 
+            size={28} 
+            color='#ff4141' 
+          />
+        </Pressable>
+      )
+    })
+  }, [route.params?.data, navigation])
+  
   return (
     <View style={styles.container}>
       <Text>Página Details</Text>
+      <Text>{route.params?.data.name}</Text>
     </View>
   )
 }
@@ -11,5 +33,5 @@ export function Details() {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'green'
-  }
+  },
 })
