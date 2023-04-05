519ea5ceb5bbda5fb29a9a1428ad8efda0e0030b
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-05 16:19:54 -0300

    animando o componente Logo e a página Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react'
 import { api } from '../../services/api'
 import { FoodList } from '../../components/FoodList'
 import { useNavigation } from '@react-navigation/native'
+import { Text as MotiText } from 'moti'
 
 export function Home() {
   const [ inputValue, setInputValue ] = useState('')
@@ -33,8 +34,42 @@ export function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <Logo />
-      <Text style={styles.title}>Encontre a receita </Text>
-      <Text style={styles.title}>que combina com você</Text>
+      <MotiText 
+        style={styles.title} 
+        from={{ 
+          opacity: 0, 
+          translateY: 15 
+        }} 
+        animate={{ 
+          opacity: 1, 
+          translateY: 0 
+        }}
+        transition={{ 
+          delay: 100, 
+          type: 'timing', 
+          duration: 650 
+        }}
+      >
+        Encontre a receita 
+      </MotiText>
+      <MotiText 
+        style={styles.title}
+        from={{ 
+          opacity: 0, 
+          translateY: 18 
+        }} 
+        animate={{ 
+          opacity: 1, 
+          translateY: 0 
+        }}
+        transition={{ 
+          delay: 200, 
+          type: 'timing', 
+          duration: 850 
+        }}
+      >
+        que combina com você
+      </MotiText>
       <View style={styles.form}>
         <TextInput 
           placeholder='Digite o nome da comida...'
