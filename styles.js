9a57e74ec961e9da16934f7345b4381de3c47e46
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-07 15:57:28 -0300

    criando um arquivo de estilização para o App

---
+++ styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    heigh: 400,
+  },
+
+  title: {
+    fontSize: 24,
+    marginTop: 100,
+  }
+});
