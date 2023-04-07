98f18e4c43b81f9b2e560b66843de84423d17f01
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-07 15:13:35 -0300

    criando um arquivo de estilização para a página Favorites

---
+++ src/pages/Favorites/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f3f9ff',
+    flex: 1,
+    paddingStart: 14,
+    paddingEnd: 14,
+    paddingTop: 36,
+  },
+
+  title: {},
+})
