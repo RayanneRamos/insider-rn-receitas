663184413c966449fe9607ff62cc1098af41d088
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-07 14:42:11 -0300

    transformando o componente FoodList em um arquivo JSX

--- src/components/FoodList/index.jsx
+++ src/components/FoodList/index.jsx
@@ -1,6 +1,7 @@
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
 import { useNavigation } from '@react-navigation/native'
+import { styles } from './styles'
 
 export function FoodList({ data }) {
   const navigation = useNavigation()
@@ -26,43 +27,3 @@ export function FoodList({ data }) {
     </TouchableOpacity>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginBottom: 14,
-  },
-
-  image: {
-    width: '100%',
-    height: 200,
-    borderRadius: 14,
-  },
-
-  info: {
-    position: 'absolute',
-    bottom: 14,
-    left: 14,
-    zIndex: 99,
-  },
-
-  name: {
-    fontSize: 18,
-    color: '#fff',
-    fontWeight: 'bold',
-  },
-
-  description: {
-    color: '#fff',
-  },
-
-  gradient: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: 0,
-    height: '55%',
-    borderRadius: 14,
-    zIndex: 1,
-    backgroundColor: 'transparent',
-  },
-})
