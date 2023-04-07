5498cfe8362238e0359db4d062ac32f28ccf3421
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-07 14:55:54 -0300

    transformando o componente Logo em um arquivo JSX

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,25 @@
+import { Text } from 'react-native'
+import { View } from 'moti'
+import { styles } from './styles'
+
+export function Logo() {
+  return (
+    <View 
+      style={styles.logoArea}
+      from={{ 
+        opacity: 0, 
+        translateX: -50 
+      }} 
+      animate={{ 
+        opacity: 1, 
+        translateX: 0 
+      }}
+      transition={{  
+        type: 'spring', 
+        duration: 850 
+      }}
+    >
+      <Text style={styles.logo}>Receita Fácil</Text>
+    </View>
+  )
+}
