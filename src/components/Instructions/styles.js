212d3ef2d830761a3f62166a1c7faa632e41a556
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-07 14:49:02 -0300

    criando um arquivo de estilização para o componente Instructions

---
+++ src/components/Instructions/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    padding: 8,
+    marginBottom: 14,
+  },
+
+  index: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+
+  text: {
+    lineHeight: 20,
+  },
+})
